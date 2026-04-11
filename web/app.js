20a1103989bf3a1fde9b1188ca52314b2bcead77
Ankan Ghosh <ghoshankan005@gmail.com>
2026-04-11 21:07:22 +0530

    Polish mobile layout spacing and nav alignment

--- web/app.js
+++ web/app.js
@@ -1828,9 +1828,6 @@ function applyDaylightTint(now = new Date()) {
 }
 
 function syncTopNavActiveButtonIntoView() {
-  if (window.matchMedia('(max-width: 700px)').matches) {
-    return;
-  }
   const activeButton = [
     els.topNavAttendanceBtn,
     els.topNavSaarthiBtn,
